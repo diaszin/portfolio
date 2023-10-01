6f8ebb04be2f55e6ce910d92dbb4e0c94d04e601
diaszin <84644190+diaszin@users.noreply.github.com>
2023-09-30 22:18:28 -0300

    Melhora paragráfo

--- src/pages/component/HeroSection.tsx
+++ src/pages/component/HeroSection.tsx
@@ -8,9 +8,9 @@ export default function HeroSection() {
       <div className="flex-row ">
           <div className="h-[28rem] flex flex-col justify-center items-start dark:border-2 dark:border-sky-200 dark:shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
             <div className="flex flex-col justify-center items-center w-full gap-3">
-                <h2 className=" text-4xl font-bold">Seja Bem Vindo !</h2>
-                <p className="w-[42rem] text-center font-mono">
-                  Olá, sou Kaique Dias, um desenvolvedor apaixonado pelo que faço. <br />   
+                <h2 className=" text-4xl font-bold font-mono">Seja Bem Vindo !</h2>
+                <p className={"w-[28rem] text-justify "+inter.className}>
+                  Olá, sou Kaique Dias, um desenvolvedor apaixonado pelo que faço.  
                   Meu compromisso vai além do simples código: acredito em valorizar o
                   processo, entender o negócio e, principalmente, criar um código limpo
                   e bem estruturado.
